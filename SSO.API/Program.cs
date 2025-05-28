9996d53d6b2876ee9e71ba9cd55925530c5f9932
chandualla <113755139+chandualla@users.noreply.github.com>
2025-05-28 23:10:27 +0530

    swagger congig

--- SSO.API/Program.cs
+++ SSO.API/Program.cs
@@ -75,12 +75,10 @@ builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
